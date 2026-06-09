2289156e0d662d4a37d04677295d77354c78fce5
Catz (CodingCat) <103620558+CodingCatz@users.noreply.github.com>
2026-06-09 21:39:42 +0800

    Class01:基本參數與編碼慣例

---
+++ Assets/Scripts/Core/GridSettings.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;//導入UNITY引擎標準程式庫(前端)
+
+//消除寶石的核心命名空間
+namespace MatchGems.Core
+{
+    /// <summary>
+    /// 棋盤格尺寸設定
+    /// </summary>
+    public class GridSettings : MonoBehaviour
+    {
+        #region 基本參數
+        [SerializeField] private int _cellSize = 64;
+        [SerializeField] private float _pixelPerUnit = 64f;
+        #endregion 基本參數
+
+        private void Start()
+        {
+            Debug.Log(_cellSize);
+        }
+
+        private void Update()
+        {
+            Debug.Log(_pixelPerUnit);
+        }
+    }
+}
