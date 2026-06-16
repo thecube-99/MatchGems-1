e7ba0c97bd72dbdae49d4b7d92bd11ff62312448
Catz (CodingCat) <103620558+CodingCatz@users.noreply.github.com>
2026-06-16 21:28:51 +0800

    Class03:寶石資料陣列建立測試(for迴圈)

--- Assets/Scripts/Core/BoardModel.cs
+++ Assets/Scripts/Core/BoardModel.cs
@@ -3,6 +3,9 @@
     public class BoardModel
     {
         #region 基本參數
+        /// <summary>
+        /// 寶石陣 (二維)
+        /// </summary>
         private GemData[,] _gems;
         #endregion 基本參數
 
@@ -42,6 +45,16 @@
             _gems[coord.X, coord.Y] = new GemData(gemType);
         }
         /// <summary>
+        /// 設定指定格子的寶石
+        /// </summary>
+        /// <param name="x">X座標</param>
+        /// <param name="y">Y座標</param>
+        /// <param name="gemType">寶石類型</param>
+        public void SetGem(int x, int y, GemType gemType)
+        {
+            _gems[x, y] = new GemData(gemType);
+        }
+        /// <summary>
         /// 取得指定格子的寶石
         /// </summary>
         /// <param name="coord">定位資料</param>
@@ -49,6 +62,16 @@
         public GemData GetGem(CellCoord coord)
         {
             return _gems[coord.X, coord.Y];
+        }
+        /// <summary>
+        /// 取得指定格子的寶石
+        /// </summary>
+        /// <param name="x">X座標</param>
+        /// <param name="y">Y座標</param>
+        /// <returns>寶石資料</returns>
+        public GemData GetGem(int x, int y)
+        {
+            return _gems[x, y];
         }
         #endregion 公開方法
     }
